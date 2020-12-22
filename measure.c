81f8288495b03abd22add9266d75a7e324ecb9fe
DorBaram <dorbaram@gmail.com>
2020-12-22 22:48:56 +0200

    dor's

--- measure.c
+++ measure.c
@@ -4,44 +4,30 @@
 
 #include<stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <arpa/inet.h>
 #include <signal.h>
 #include <netinet/tcp.h>
 #include <time.h>
 
 #define SERVER_PORT 12345  //The port that the server listens
 #define LENGTH 65536
-#define FILESIZE 1048576
-  
-int main()
-{
 
-signal(SIGPIPE, SIG_IGN); // on linux to prevent crash on closing socket
-
+int main() {
+    signal(SIGPIPE, SIG_IGN); // on linux to prevent crash on closing socket
     // Open the listening (server) socket
-    int sock;  
-	sock = socket(AF_INET , SOCK_STREAM , 0 );
-
-    if(sock == -1) 
-    {
-        printf("Could not create listening socket : %d" ,errno);
-    }
-
-     printf ("measure: open socket %d succesfuly /n", sock);
-
-	// Reuse the address if the server socket on was closed
-	// and remains for 45 seconds in TIME-WAIT state till the final removal.
+    int sock;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) { printf("Could not create listening socket : %d", errno); }
+    printf("measure: open socket %d successfully /n", sock);
+    // Reuse the address if the server socket on was closed
+    // and remains for 45 seconds in TIME-WAIT state till the final removal.
 
     int enableReuse = 1;
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(int)) < 0)
-    {
-         printf("setsockopt() failed with error code : %d" , errno);
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(int)) < 0) {
+        printf("setsockopt() failed with error code : %d", errno);
     }
 
     // "sockaddr_in" is the "derived" from sockaddr structure
@@ -54,69 +40,68 @@ signal(SIGPIPE, SIG_IGN); // on linux to prevent crash on closing socket
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(SERVER_PORT);  //network order
 
-      
     // Bind the socket to the port with any IP at this port
-    if (bind (sock, (struct sockaddr *)&serverAddress , sizeof(serverAddress)) == -1)
-    {
-        printf("Bind failed with error code : %d" , errno);
+    if (bind(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1) {
+        printf("Bind failed with error code : %d", errno);
 
-	// TODO: close the socket
+        // TODO: close the socket
         return -1;
     }
-      
+
     printf("Bind() success\n");
-  
+
     // Make the socket listening; actually mother of all client sockets.
     if (listen(sock, 500) == -1) //a Maximum size of queue connection requests
-											//number of concurrent connections 
+        //number of concurrent connections
     {
-	printf("listen() failed with error code : %d \n",errno);
-	// TODO: close the socket
+        printf("listen() failed with error code : %d \n", errno);
+        // TODO: close the socket
         return -1;
     }
-      
+
     //Accept and incoming connection
     printf("Waiting for incoming TCP-connections...\n");
 
-    int clientSocket; 
-    struct sockaddr_in clientAddress;  
+    int clientSocket;
+    struct sockaddr_in clientAddress;
     socklen_t addressLen = sizeof(clientAddress);
 
-   
     memset(&clientAddress, 0, sizeof(clientAddress));
     addressLen = sizeof(clientAddress);
 
     time_t startC, endC;
-    clientSocket = accept(sock, (struct sockaddr *)&clientAddress, &addressLen);
-    if (clientSocket == -1)
-    {
-        printf("accept failed with error code : %d",errno);
-	   // TODO: close the sockets
-           return -1;
-    	}
-    
-    printf ("sender connected succsesfuly \n");
+
+
+    printf("sender connected succsesfuly \n");
     startC = time(NULL);
-    char filename[LENGTH];
+    char fileBuffer[LENGTH];
 
-    int stream =0;
-    int i =0;
-    while (i<5){
-    while (stream = recv(clientSocket, filename, LENGTH, 0))  {
-      if (stream == -1){
-           printf("accept failed with error code : %d",errno);
-	   // TODO: close the sockets
-           return -1;
-      }
+    int stream = 0;
+    int i = 0;
+    memset(&fileBuffer, 0, sizeof(fileBuffer));
+    while (i < 5) {
 
+        clientSocket = accept(sock, (struct sockaddr *) &clientAddress, &addressLen);
+        if (clientSocket == -1) {
+            printf("accept failed with error code : %d", errno);
+            // TODO: close the sockets
+            return -1;
+        }
+
+
+
+        while (stream = recv(clientSocket, fileBuffer, LENGTH, 0)) {
+            printf("stream is %d \n",stream);
+            if (stream == -1) {printf("accept failed with error code : %d", errno);
+                // TODO: close the sockets
+                return -1;}
         }
         i++;
-     printf ("measur recived %d times \n", i);
-  
+        printf("measure received %d times \n", i);
     }
     endC = time(NULL);
 
-    printf ("The time of cubic is %.4f\n", (difftime(endC, startC))); 
+    printf("The time of cubic is %.4f\n", (difftime(endC, startC)));
 
 
     /* change CC */
@@ -126,39 +111,39 @@ signal(SIGPIPE, SIG_IGN); // on linux to prevent crash on closing socket
 
     strcpy(buf, "reno");
     len = strlen(buf);
-  
+
 
     if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
-        perror("setsockopt"); 
+        perror("setsockopt");
         return -1;
     }
-    printf ("Change to reno \n");
-    
+    printf("Change to reno \n");
+
     time_t startR, endR;
 
     startR = time(NULL);
 
-    stream =0;
-     i =0;
-    while (i<5){
-    while (stream = recv(clientSocket, filename, LENGTH, 0))  {
-      if (stream == -1){
-           printf("accept failed with error code : %d",errno);
-	   // TODO: close the sockets
-           return -1;
-      }
-
+    stream = 0;
+    i = 0;
+    while (i < 5) {
+        memset(&fileBuffer, 0, sizeof(fileBuffer));
+        while (stream = recv(clientSocket, fileBuffer, LENGTH, 0)) {
+            if (stream == -1) {
+                printf("accept failed with error code : %d", errno);
+                // TODO: close the sockets
+                return -1;
+            }
+            printf("stream is %d",stream);
         }
         i++;
-     printf ("measur recived %d times \n", i);
-  
+        printf("measur recived %d times \n", i);
     }
     endR = time(NULL);
     float timeCaount = difftime(endR, startR);
-    printf ("The time of reno is %.4f \n", timeCaount); 
+    printf("The time of reno is %.4f \n", timeCaount);
 
     // TODO: All open clientSocket descriptors should be kept
     // in some container and closed as well.
-    close(sock);              
+    close(sock);
     return 0;
 }
--- sender.c
+++ sender.c
@@ -1,94 +1,81 @@
 /*
 	TCP/IP client
-*/ 
+*/
 
 #include <stdio.h>
 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <string.h> 
-#include <sys/types.h> 
+#include <errno.h>
+#include <string.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <libgen.h>
 
-//EOF
-//1048576
 #define SERVER_PORT 12345
 #define SERVER_IP_ADDRESS "127.0.0.1"
 #define LENGTH 65536
-#define FILESIZE 1048576
 
-
-int main()
-{
-    char buffer[LENGTH]; 
+int main() {
+    char buffer[LENGTH];
     int sock;
 
-     sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
     //socket- return socket or -1, AF_INET- struct of ipV4, SOCK_STREAM- struct of TCP
-     if(sock == -1)
-    {
-        printf("Could not create socket : %d \n" ,errno);
+    if (sock == -1) {
+        printf("Could not create socket : %d \n", errno);
     }
-    printf ("sender: open socket %d succesfuly \n", sock);
+    printf("sender: open socket %d succesfuly \n", sock);
 
     struct sockaddr_in serverAddress; //sockaddr_in- socket internet address for ipV4 (family(ipv4), port, internet address)
     memset(&serverAddress, 0, sizeof(serverAddress)); //reset the adder
 
     serverAddress.sin_family = AF_INET; //ip and port dest
     serverAddress.sin_port = htons(SERVER_PORT); //the server port in byte
-	int rval = inet_pton(AF_INET, (const char*)SERVER_IP_ADDRESS, &serverAddress.sin_addr); 
+    int rval = inet_pton(AF_INET, (const char *) SERVER_IP_ADDRESS, &serverAddress.sin_addr);
     //convert to bit and return if failed
-	if (rval <= 0)
-	{
-		printf("inet_pton() failed \n");
-		return -1;
-	}
-
-     // Make a connection to the server with socket SendingSocket.
-     int conn =  connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)); 
-     //return -1 if connect failed
-     if (conn == -1)
-     {
-	   printf("connect() failed with error code : %d \n" ,errno );
-     }
-
-     printf("sender: connected to server succesfuly %d \n", conn);
-
-     
-   
-    /* Send File 5 times */
-    int i =0;
-    while (i < 5){
-
-     char* file_name = "/home/user/Tikshoret_ex3/1mb.txt";
-        char sbuffer[LENGTH]; 
-		FILE *file = fopen(file_name, "r");
-		if(file == NULL)
-		{
-		    printf("ERROR:File %s not found.\n", file_name);
-			return 1;
-		}
-
-		int stream; 
-		while((stream = fread(sbuffer, sizeof(char), LENGTH, file))>0) {
-            int sending = send(sock, sbuffer, stream, 0);
-	        if( sending < 0)
-	        {
-	            printf("send() failed with error code : %d"  ,errno);
-                return 1;
-	        }
-	    }
-        fclose(file);
-         i++;
-	    printf("Sender Sent the file %d time success!\n", i);
-       
+    if (rval <= 0) {
+        printf("inet_pton() failed \n");
+        return -1;
     }
-  
+
+    // Make a connection to the server with socket SendingSocket.
+    int conn = connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+    //return -1 if connect failed
+    if (conn == -1) {
+        printf("connect() failed with error code : %d \n", errno);
+    }
+
+    printf("sender: connected to server succesfuly %d \n", conn);
+
+    /* Send File 5 times */
+    int i = 0;
+    while (i < 5) {
+
+        char *file_name = "/home/user/Desktop/New Tikshoret ex3/Tikshoret_ex3/1mb.txt";
+        char sbuffer[LENGTH];
+        FILE *file = fopen(file_name, "r");
+        if (file == NULL) {
+            printf("ERROR:File %s not found.\n", file_name);
+            return 1;
+        }
+        int sending;
+        int stream;
+        while ((stream = fread(sbuffer, sizeof(char), LENGTH, file)) > 0) {
+            sending = send(sock, sbuffer, stream, 0);
+            if (sending < 0) {
+                printf("send() failed with error code : %d", errno);
+                return 1;
+            }
+           // printf("stream is %d",stream);
+           // printf("sending is %d",sending);
+        }
+        fclose(file);
+        i++;
+        printf("Sender Sent the file %d time success!\n", i);
+        memset(&sbuffer, 0, sizeof(sbuffer)); //reset the buffer
+    }
+
     /* change CC */
 
     char buf[LENGTH];
@@ -96,48 +83,48 @@ int main()
 
     strcpy(buf, "reno");
     len = strlen(buf);
-  
+
 
     if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
-        perror("setsockopt"); 
+        perror("setsockopt");
         return -1;
     }
-    printf ("Change to reno \n");
+    printf("Change to reno \n");
 
-    
-   
+
+
     /* Send File 5 times */
-     i =0;
-    while (i < 5){
+    i = 0;
+    while (i < 5) {
 
-     char* file_name = "/home/user/Tikshoret_ex3/1mb.txt";
-        char sbuffer[LENGTH]; 
-		FILE *file = fopen(file_name, "r");
-		if(file == NULL)
-		{
-		    printf("ERROR: File %s not found.\n", file_name);
-			return 1;
-		}
+        char *file_name = "/home/user/Desktop/New Tikshoret ex3/Tikshoret_ex3/1mb.txt";
+        char sbuffer[LENGTH];
+        FILE *file = fopen(file_name, "r");
+        if (file == NULL) {
+            printf("ERROR: File %s not found.\n", file_name);
+            return 1;
+        }
 
-		int stream; 
-		while((stream = fread(sbuffer, sizeof(char), LENGTH, file))>0) {
+        int stream;
+        while ((stream = fread(sbuffer, sizeof(char), LENGTH, file)) > 0) {
             int sending = send(sock, sbuffer, stream, 0);
-	        if( sending < 0)
-	        {
-	            printf("send() failed with error code : %d"  ,errno);
+            if (sending < 0) {
+                printf("send() failed with error code : %d", errno);
                 return 1;
-	        }
-	    }
+            }
+           // printf("stream is %d",stream);
+            //printf("sending is %d",sending);
+        }
         fclose(file);
         i++;
-	    printf("Sender Sent the file %d time success!\n", i);
-       
+        printf("Sender Sent the file %d time success!\n", i);
+        memset(&sbuffer, 0, sizeof(sbuffer)); //reset the buffer
     }
 
-	 sleep(3);
- 
+    sleep(3);
+
 
     close(sock);
-    
-     return 0;
+
+    return 0;
 }
